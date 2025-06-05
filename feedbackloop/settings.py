3d600e582e997c4092711ff7a2dfce79745cf775
Ernosto0 <ernosto20.03@gmail.com>
2025-06-05 15:32:46 +0300

    Local false

--- feedbackloop/settings.py
+++ feedbackloop/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default-key-for-development')
 
-ONLOCALHOST = True
+ONLOCALHOST = False
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False  # Set to True for development
